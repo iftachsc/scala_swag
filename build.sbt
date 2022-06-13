7971b980488c8b003b30e070b9585779e28ccc40
Iftach Schonbaum <iftach.schonbaum@gmail.com>
2022-06-13 08:57:27 +0300

    working swag

--- build.sbt
+++ build.sbt
@@ -1,34 +1,45 @@
-
-// The simplest possible sbt build file is just one line:
-
 scalaVersion := "2.13.8"
-// That is, to create a valid sbt build, all you've got to do is define the
-// version of Scala you'd like your project to use.
-
-// ============================================================================
-
-// Lines like the above defining `scalaVersion` are called "settings". Settings
-// are key/value pairs. In the case of `scalaVersion`, the key is "scalaVersion"
-// and the value is "2.13.8"
-
-// It's possible to define many kinds of settings, such as:
 
 name := "hello-world"
 organization := "ch.epfl.scala"
 version := "1.0"
 
-// Note, it's not required for you to define these three settings. These are
-// mostly only necessary if you intend to publish your library's binaries on a
-// place like Sonatype.
-
-
-// Want to use a published library in your project?
-// You can define other libraries as dependencies in your build like this:
+val zioVersion = "1.0.12"
+val htt4sVersion = "0.21.22"
+val circeVersion = "0.14.2"
 
 libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "2.1.1"
-libraryDependencies += "org.json4s"             %% "json4s-native"            % "4.0.5"
+//libraryDependencies += "org.json4s"             %% "json4s-native"            % "4.0.5"
 
-libraryDependencies += "dev.zio" %% "zio" % "1.0.12"
-libraryDependencies += "dev.zio" %% "zio-streams" % "1.0.12"
-libraryDependencies += "dev.zio" %% "zio-process" % "0.6.1"
+libraryDependencies ++= Seq(
+    "dev.zio" %% "zio",
+    "dev.zio" %% "zio-streams",
+    
+).map(_ % zioVersion)
+
+
 libraryDependencies += "dev.zio" %% "zio-json" % "0.1.4"
+libraryDependencies += "dev.zio" %% "zio-json-interop-http4s" % "0.1.5"
+
+libraryDependencies ++= Seq(
+    "org.http4s" %% "http4s-blaze-server",
+    "org.http4s" %% "http4s-circe",
+    "org.http4s" %% "http4s-dsl"
+).map(_ % htt4sVersion)
+
+libraryDependencies ++= Seq(
+  "io.circe" %% "circe-generic",
+  "io.circe" %% "circe-literal",
+  "io.circe" %% "circe-generic-extras",
+  "io.circe" %% "circe-parser"
+
+ 
+).map(_ % circeVersion)
+
+libraryDependencies += "dev.zio" %% "zio-interop-cats" % "3.2.9.1"
+
+//libraryDependencies += "dev.zio" %% "zio-process" % "0.6.1"
+
+
+
+
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,9 @@
-//import scala.concurrent.duration.Duration
-import homework.hunters.io.operator._
 
-import zio.console._
-import zio.process._
+import zio.console
 import zio._
 import java.time
 import zio.stream._
-import zio.duration.Duration
+import zio.duration
 import java.util.concurrent.TimeUnit
 import zio.json._
 import zio.stream.ZStream.TerminationStrategy
@@ -26,74 +23,24 @@ object ZioMain extends zio.App {
   def run(args: List[String]) =
     source.exitCode
   
-  // val myAppLogic = 
-  //   for {
-  //     fiber <-  Command("/Users/yftach.shenbaum/Downloads/blackbox").linesStream
-  //     _       <- ZIO.sleep(duration.`package`.Duration.fromMillis(5000))
-  //     _       <- process.kill
-  //   } yield ()
+  val source = {
 
-  
+    val windowSize = 10.seconds
+    val slide = 2.seconds
 
-  
-  
-  
-  
-  val source = for {
-     now <- currentTime(TimeUnit.MILLISECONDS)
-     _ <- putStrLn(now.toString)
-     //_ <- SimpleSwag partialAggregationsByEventTime(DataGen.blackbox.map(x => x.fromJson[WordEvent]).collectRight,4.seconds,now.millis, 1.second)
-     _ <- SimpleSwag.partialAggregationsByEventTime(DataGen.blackbox.map(x => x.fromJson[WordEvent]).collectRight, _.timestamp ,4.seconds, 1.second)
-  } yield ()
-
-  
-  // val source = for {
-  //               grace <- ZIO.succeed(2.seconds)
-  //               slide <- ZIO.succeed(6.seconds)
-  //               now <- currentTime(TimeUnit.SECONDS).tap(x => putStrLn(x.toString))
-  //               //fiber <- (1 to volumeScale).foldLeft(command.linesStream)((x,_) => x.merge(command.linesStream,TerminationStrategy.Both))
-  //               fiber     <- blackbox
-  //                             .tap(x => putStrLn(s"Count $x"))
-  //                             .map(x => x.fromJson[WordEvent]).collectRight
-  //                             .runCollect
-
-  //               _ <- partialAggregationsByEventTime(fiber, 3.seconds, now.seconds)
-  //                               // .takeWhile(_.timestamp <= now + slide.getSeconds)
-  //                               // .aggregateAsyncWithin(ZTransducer.foldLeft(0)((x,_) => x + 1)
-  //                               //   ,Schedule.fixed(slide + grace))//limitting grace time for late arrivals
-                                
-  //                             //.take(2)
-  //                              // .aggregateAsync(ZTransducer.foldLeft(0)(_ + 1))
-  //                               // .tap(x => 
-  //                               //   putStrLn(s"Count $x"))
-  //                                 //.runHead
-  //               _ <- putStrLn("its running!")
-  //               // _ <- fiber.join
-  //             } yield ()
-    
-  
-  
-  
-
-
-  // val command = "/Users/yftach.shenbaum/Downloads/blackbox"
-
-  // val sourceOperator      = StdoutSourceOperator[WordEvent](fac = data => WordEvent(data), command, parallelism = 4)
-  // //val mapOperator         = MapOperator[WordEvent,(Long, String, Int)](sourceOperator.stream, x => (x.timestamp, x.event_type + "-" + x.data,1))
-  // val keyByOperator       = KeyByOperator[WordEvent](x => x.key, sourceOperator.stream, parallelism = 1)
-
-  // val windowByKeyOperator = WindowByKeyOperator(keyByOperator.streams, windowSize = Duration("20 seconds"), slide = Duration("5 seconds"))
-  
-  
-  // val sink                = StdOutSinkOperator(windowByKeyOperator.stream, block = false)   
-
-  // while(true){
-  //   println(windowByKeyOperator.queryState(("baz","dolor")))
-  //   Thread.sleep(3000)
-  // }
-  // println("Reached the end of the program")
-
-  
+    for {
+      now  <- currentTime(TimeUnit.MILLISECONDS)
+      //_ <- Http4sServer.server
+      state <- SimpleSwag.partialAggregationsByEventTime(
+                  DataGen.blackbox.map(_.fromJson[WordEvent]).collectRight, 
+                  _.timestamp,
+                  windowSize,
+                  slide,
+                  1.second)
+      
+                //).catchAll(_ => IO.fail("Something went wrong computing sliding window"))
+    } yield ()
+  }
 }
 
  
